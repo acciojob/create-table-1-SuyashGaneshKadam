b7ed3509d59cb713992429ecab1349a4803f95f9
Suyash Kadam <122480150+SuyashGaneshKadam@users.noreply.github.com>
2023-10-25 13:50:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 function insert_Row() {
     //Write your code here
-  
-  
+	let table = document.getElementById("sampleTable");
+	let tr = document.createElement("tr");
+	
+	let cell1 = document.createElement("td");
+	cell1.innerText = "New Cell1";
+	
+	let cell2 = document.createElement("td");
+	cell2.innerText = "New Cell2";
+
+	tr.append(cell1, cell2);
+
+	table.insertBefore(tr, table.children[0]);
 }

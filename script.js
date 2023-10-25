40b079d63574f68ef2646a970244d644d27817b6
Suyash Kadam <122480150+SuyashGaneshKadam@users.noreply.github.com>
2023-10-25 17:45:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,15 +1,21 @@
-function insert_Row() {
-    //Write your code here
+// function insert_Row() {
+//     //Write your code here
+// 	let table = document.getElementById("sampleTable");
+// 	let tr = document.createElement("tr");
+	
+// 	let cell1 = document.createElement("td");
+// 	cell1.innerText = "New Cell1";
+	
+// 	let cell2 = document.createElement("td");
+// 	cell2.innerText = "New Cell2";
+
+// 	tr.append(cell1, cell2);
+	
+// 	table.insertBefore(tr, table.children[0]);
+// }
+
+function insert_Row(){
 	let table = document.getElementById("sampleTable");
-	let tr = document.createElement("tr");
-	
-	let cell1 = document.createElement("td");
-	cell1.innerText = "New Cell1";
-	
-	let cell2 = document.createElement("td");
-	cell2.innerText = "New Cell2";
-
-	tr.append(cell1, cell2);
-
-	table.insertBefore(tr, table.children[0]);
+	let x = `<tr><td>New Cell1</td><td>New Cell2</td></tr>`;
+	table.innerHTML = x + table.innerHTML;
 }
